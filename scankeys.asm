936fda141bcec24dd2cb4904e98d1876b1672779
Dave Van Wagner <git@davevw.com>
2024-05-20 22:08:39 -0700

    display characters and scan code values, credit

--- scankeys.asm
+++ scankeys.asm
@@ -17,16 +17,45 @@ color=$286 ; 646 decimal
 zvideo=$fb
 zcolor=$fd
 clearscreen=147
+scancode=$26
+found=$27
+savex=$28
+keycounter=$ff
+kbdmatrix=$eb81
 
 *=$c000
-	lda #clearscreen
-	jsr CHROUT
+	lda #<title
+	ldx #>title
+	jsr strout
 	sei
 	ldx #$ff
 	stx CIDDRA ; set port A as 8 outputs
 	inx
 	stx CIDDRB ; set port B as 8 inputs
-	ldy #1 ; first screen row, second column for heading
+
+	lda #'D'-64+128 ; to screen code, and reverse
+	ldy color
+	sta videoram+4
+	sty colorram+4
+	sta videoram+120
+	sty colorram+120
+	lda #'C'-64+128
+	sta videoram+5
+	sty colorram+5
+	sta videoram+160
+	sty colorram+160
+	lda #'0'+128
+	sta videoram+6
+	sty colorram+6
+	sta videoram+7
+	sty colorram+7
+	sta videoram+200
+	sty colorram+200
+	lda #'1'+128
+	sta videoram+240
+	sty colorram+240
+
+	ldy #42 ; second screen row, third column for heading
 	lda #'7'
 -	sta videoram,y
 	pha
@@ -40,59 +69,75 @@ clearscreen=147
 	bne - ; branch if more digits
 	ldx #'0' ; row heading
 	ldy color
-	stx videoram+40
-	sty colorram+40
+	stx videoram+81
+	sty colorram+81
 	inx
-	stx videoram+80
-	sty colorram+80
+	stx videoram+121
+	sty colorram+121
 	inx
-	stx videoram+120
-	sty colorram+120
+	stx videoram+161
+	sty colorram+161
 	inx
-	stx videoram+160
-	sty colorram+160
+	stx videoram+201
+	sty colorram+201
 	inx
-	stx videoram+200
-	sty colorram+200
+	stx videoram+241
+	sty colorram+241
 	inx
-	stx videoram+240
-	sty colorram+240
+	stx videoram+281
+	sty colorram+281
 	inx
-	stx videoram+280
-	sty colorram+280
+	stx videoram+321
+	sty colorram+321
 	inx
-	stx videoram+320
-	sty colorram+320
---- lda #<(videoram+41)
+	stx videoram+361
+	sty colorram+361
+--- lda #<(videoram+82)
 	sta zvideo
 	sta zcolor
-	lda #>(videoram+41)
+	lda #>(videoram+82)
 	sta zvideo+1
-	lda #>(colorram+41)
+	lda #>(colorram+82)
 	sta zcolor+1
-	lda #$fe ; start with bit 1 grounded
---	ldy #$00 ; counter
+
+	; initialize scancode variables
+	lda #63
+	sta keycounter ; count for scan codes
+	sta found
+	lda #64
+	sta scancode
+
+	lda #$7F ; start with bit 7 grounded
+--	ldy #0 ; count number of scanlines
 	sta CIAPRA ; output keyboard scanline
 	ldx CIAPRB ; read keys pressed
 	pha ; save scanline
 	txa
 -	asl ; shift keys into carry one by one
 	pha ; save keys
-	lda #'*' ; pressed
-	bcc +
 	lda #' ' ; not pressed
+	bcs +
+	; bit found
+	; bmi alreadyfound
+	lda keycounter
+	sta scancode
+	sec
+	ror found ; mark found
+alreadyfound:	
+	jsr lookupkey
 +	sta (zvideo),y ; videoram pointer for key
 	lda color
 	sta (zcolor),y ; colorram pointer for key
 	pla ; restore keys
+	dec keycounter
 	iny
 	cpy #8 ; completed the row?
 	bne - ; branch if more on row
 	pla ; restore scanline
-	sec 
-	rol ; inject bit while shifting
+	sec
+	ror ; inject bit while shifting
 	cmp #$ff ; check for all bits set
-	beq --- ; if done, restart scanning
+	beq ++ ; branch if done
 	pha ; save scanline
 	clc
 	lda zvideo
@@ -108,3 +153,96 @@ clearscreen=147
 	inc zcolor+1
 +	pla ; restore scanline
 	jmp --
+++	lda scancode
+	lsr
+	lsr
+	lsr
+	lsr
+	ora #$B0 ; reverse zero
+	cmp #$BA ; out of bounds for numeric?
+	bcc + ; branch if okay
+	adc #$C6 ; convert to A..F
++   sta videoram+1
+	lda color	
+	sta colorram
+	sta colorram+1
+	sta colorram+2
+	lda #'$'
+	sta videoram
+	lda scancode
+	and #$0F
+	ora #$B0
+	cmp #$BA
+	bcc +
+	adc #$C6
++	sta videoram+2
+	lda #'#'
+	sta videoram+10
+	lda scancode
+	jsr div10
+	ora #$30
+	sta videoram+12
+	txa
+	ora #$30
+	sta videoram+11
+	ldy color
+	sty colorram+10
+	sty colorram+11
+	sty colorram+12
+	jmp ---
+lookupkey:
+	stx savex
+	ldx keycounter
+	lda kbdmatrix, x
+	cmp #$20
+	bcs +
+	cmp #1
+	bne +
+	lda #$9E ; Reverse up arrow
+	bne ++
++	cmp #2
+	bne +
+	lda #'='+128
+	bne ++
++	cmp #4
+	bne +
+	lda #3+128
+	bne ++
++	cmp #$20
+	bcs +
+	ora #$80
+	bne ++
++	cmp #$80
+	bcc +
+	ora #$40
+	bne ++
++	cmp #$40
+	bcc ++
+	eor #$40	
+++	ldx savex
+	rts
+div10: ; take A and divide by 10, remainder in A, answer in X
+	ldx #0
+-	cmp #10
+	bcc +
+	sbc #10
+	inx
+	bne -
++	rts
+strout:
+	sta $fb
+	stx $fc
+	ldy #0
+-	lda ($fb),y
+	beq +
+	jsr CHROUT
+	iny
+	bne -
++	rts
+
+title: 
+	!byte 147
+	!byte 17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17,17
+	!text 157,157,17,18,32,157,148,32,145,13
+	!text 18,"SCANKEYS (C) 2024 BY GITHUB.COM/DAVERVW"
+	!byte 0
